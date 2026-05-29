26ac59801871bb14a5709bd4b23c7d69b4025532
istalo <istalo1@hotmail.com>
2026-05-29 01:24:25 -0300

    🍱 Adiciona rota de vendas

---
+++ src/routes/vendas.ts
@@ -0,0 +1,85 @@
+import { prisma } from "../../lib/prisma"
+import { Router } from "express"
+import { z } from "zod"
+import { TipoPag } from "../../generated/prisma/enums";
+
+const router = Router()
+
+export const vendaSchema = z.object({
+    clienteId: z.number().int()
+      .positive('ID do cliente deve ser um número positivo'),
+    formaPag: z.enum(TipoPag),
+    total: z.number()
+      .positive('Total da venda deve ser um número positivo'),
+})
+
+router.get("/", async (req, res) => {
+    try {
+        const vendas = await prisma.venda.findMany({
+            include: { cliente: true }
+        })
+        res.status(200).json(vendas)
+    } catch (error) {
+        res.status(500).json({ erro: "Erro no servidor" })
+    }
+})
+
+router.post("/", async (req, res) => {
+    const valida = vendaSchema.safeParse(req.body)
+
+    if (!valida.success) {
+        res.status(400).json({ erro: valida.error })
+        return
+    }
+
+    const { clienteId, formaPag, total } = valida.data
+
+    const cliente = await prisma.cliente.findUnique({
+        where: { id: clienteId }
+    })
+
+    if (!cliente) {
+        res.status(404).json({ erro: 'Cliente não cadastrado' })
+        return
+    }
+
+    try {
+        // Cria apenas a venda, sem tentar atualizar o cliente
+        const venda = await prisma.venda.create({ 
+            data: { clienteId, formaPag, total }
+        });
+        
+        res.status(201).json(venda);
+    } catch (error) {
+        console.error(error); // Imprime o erro completo no seu terminal!
+        res.status(500).json({ erro: "Erro ao criar a venda." });
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const vendaExcluida = await prisma.venda.findUnique(
+            { where: { id: Number(id)} }
+        )
+
+    if (!vendaExcluida) {
+        res.status(404).json({ erro: "Venda não encontrada." });
+        return;
+      }
+        const [venda,cliente] = await prisma.$transaction([
+            prisma.venda.delete({ where: { id: Number(id)} }),
+            prisma.cliente.update({
+              data: { saldo: { decrement: vendaExcluida?.total }},
+              where: { id: vendaExcluida?.clienteId }
+           })
+        ])
+
+        res.status(200).json({venda: venda, cliente})
+    } catch (error) {
+        res.status(500).json({ erro: error })
+    }
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,15 @@ const port = 3000
 
 import routesClientes from "./routes/clientes"
 import routesProdutos from "./routes/produtos"
-// import routesVendas from "./routes/vendas"
+import routesVendas from "./routes/vendas"
 // import routesItensVenda from "./routes/itens_venda"
 
 app.use(express.json())
 
 app.use("/clientes", routesClientes)
 app.use("/produtos", routesProdutos)
+app.use("/vendas", routesVendas)
 // app.use("/itens_venda", routesItensVenda)
-// app.use("/vendas", routesVendas)
 
 app.get('/', (req, res) => {
   res.send('Sistema de Controle de Estoque - InfoSul')
